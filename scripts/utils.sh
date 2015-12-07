1629a37284aa83ae4546ca43c2573907b9889ab6
Aditya Anchuri <aanchuri@pivotal.io>
2015-12-07 10:56:24 -0800

    Changed scripts in cf-mysql master pipeline to read creds and other info from lock metadata file for initial environment testing
    [#109462164]

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -5,3 +5,14 @@ function check_if_deployment_exists () {
     deployments_output=$( bosh deployments )
     echo ${deployments_output} | grep ${DEPLOYMENT_NAME}
 }
+
+function read_vars_from_env_lock_file () {
+    lock=$(cat ${1})
+    lock_keys=$(echo "${lock}" | jq -r keys | tr -d '"' | tr -d ',' | tr -d '[' | tr -d ']')
+    for i in $lock_keys
+    do
+      key_uc=$(echo $i | tr '[:lower:]' '[:upper:]')
+      val=$(echo "${lock}" | jq -r .${i})
+      echo "$key_uc=$val"
+    done
+}
