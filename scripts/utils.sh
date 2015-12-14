c28d93962878cf06cdf5201f7b0eb0584022eb13
Lyle Franklin <lfranklin@pivotal.io>
2015-12-14 12:28:31 -0800

    Update test-dashboard to pull creds from lock file
    [#110007158]

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -6,17 +6,6 @@ check_if_deployment_exists () {
     echo ${deployments_output} | grep ${DEPLOYMENT_NAME}
 }
 
-read_vars_from_env_lock_file () {
-    lock=$(cat ${1})
-    lock_keys=$(echo "${lock}" | jq -r keys | tr -d '"' | tr -d ',' | tr -d '[' | tr -d ']')
-    for i in $lock_keys
-    do
-      key_uc=$(echo $i | tr '[:lower:]' '[:upper:]')
-      val=$(echo "${lock}" | jq -r .${i})
-      echo "export $key_uc=$val"
-    done
-}
-
 jq_val() {
     JSON_KEY="${1:?Expected key name as first arg}"
     JSON_FILE="${2:?Expected JSON file as second arg}"
