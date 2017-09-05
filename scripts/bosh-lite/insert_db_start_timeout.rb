1931009631ac2f93c5d7655e8c82a89eaf6169c8
Joseph Palermo <jpalermo@pivotal.io>
2017-09-04 19:22:33 -0700

    Remove unused scripts; remove ondemand suffix where original no longer exists

--- scripts/bosh-lite/insert_db_start_timeout.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'yaml'
-filename = ARGV.first
-manifest = YAML::load_file(filename)
-manifest['properties']['cf_mysql']['mysql']['database_startup_timeout'] = 600
-File.open(filename, 'w'){|f| f.write(YAML::dump(manifest))}
