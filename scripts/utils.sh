eaf165a4a3c94d104f02a1dd2b4b649bcd1dd90e
Caroline Taymor <ctaymor@pivotal.io>
2018-08-23 16:18:23 -0700

    cf_uaa_admin_client_secret should use varstore or credhub only
    There is not a reasonable CI case where we would not have **either** a
    varstore or a credhub. (It is possible we might have credhub and not be
    logged in though). So we think we only need two cases on this
    conditional.
    
    Signed-off-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -118,10 +118,8 @@ cf_uaa_admin_client_secret() {
 
     if [ -f varstore/deployment-vars.yml ]; then
       echo "$(bosh int varstore/deployment-vars.yml --path /uaa_admin_client_secret)"
-    elif [ -d "${local_workspace}/environment" ]; then
-      echo "$(credhub_value cf/uaa_admin_client_secret)"
     else
-      echo "admin-secret"
+      echo "$(credhub_value cf/uaa_admin_client_secret)"
     fi
 
 }
