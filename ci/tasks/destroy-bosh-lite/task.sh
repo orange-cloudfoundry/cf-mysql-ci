68fc552282f211e9fad4f848136620920f921770
Andrew Garner <agarner@pivotal.io>
2018-12-19 14:10:46 -0800

    Revert "Fix destroy-bosh-lite task"
    This reverts commit 5d91e6f4aa4bcb4f88d1cc1c7e6e2022d4f8e551.

--- ci/tasks/destroy-bosh-lite/task.sh
+++ ci/tasks/destroy-bosh-lite/task.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -eu
 
-bosh delete-env "bosh-lite-info/manifest.yml" \
-  --state "bosh-lite-info/bosh-state.json" \
-  --vars-store "bosh-lite-info/bosh-creds.yml"
+my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ci_dir="$( cd "${my_dir}/../../" && pwd )"
+workspace_dir="$( cd "${ci_dir}/../" && pwd )"
+
+BOSH_LITE_INFO_DIR="${workspace_dir}/${BOSH_LITE_INFO_DIR}"
+
+bosh delete-env "${BOSH_LITE_INFO_DIR}/manifest.yml" \
+  --state "${BOSH_LITE_INFO_DIR}/bosh-state.json" \
+  --vars-store "${BOSH_LITE_INFO_DIR}/bosh-creds.yml"
 
