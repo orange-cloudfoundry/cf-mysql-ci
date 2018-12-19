6fa876d3b3b35f7f7cf5bf791701cd54fd82ef0e
Chris Tarazi <tarazichris@gmail.com>
2018-12-18 23:02:06 -0800

    Fix delete-stemcell script
    The previous commit which introdoces the new task.yml pattern overlooked
    updating the relative paths used in the script. The commit is 40317bc.
    
    [#162721103]

--- ci/tasks/delete-stemcell/task.sh
+++ ci/tasks/delete-stemcell/task.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -eux
 
-my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-workspace_dir="$( cd "${my_dir}/../../../" && pwd )"
-bosh_lite_dir="deployments/bosh-lite-gcp"
-
-service_key="${bosh_lite_dir}/ci-service-account.json"
-
-gcloud auth activate-service-account --key-file=${service_key}
+gcloud auth activate-service-account \
+    --key-file="./deployments/bosh-lite-gcp/ci-service-account.json"
 gcloud config set project cf-core-services
 
-stemcell_id=$(bosh int "${workspace_dir}/bosh-lite-info/bosh-state.json" --path /stemcells/0/cid)
+stemcell_id=$(bosh int "./bosh-lite-info/bosh-state.json" --path /stemcells/0/cid)
 
 echo "Deleting stemcell ${stemcell_id}"
 
@@ -18,9 +13,9 @@ gcloud compute images delete \
         ${stemcell_id} \
         --quiet
 
-cp ${workspace_dir}/bosh-lite-info/* ${workspace_dir}/bosh-lite-info-stemcell-deleted/
+cp ./bosh-lite-info/* ./bosh-lite-info-stemcell-deleted/
 
-pushd "${workspace_dir}/bosh-lite-info-stemcell-deleted"
+pushd "./bosh-lite-info-stemcell-deleted"
     cat bosh-state.json | jq "del(.stemcells[0])" > bosh-state.json.new
     mv bosh-state.json.new bosh-state.json
 popd
